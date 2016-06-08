78694c294a9c01ea9cf83cb975e0bbe885865642
Alicia Beltran <aliceresponde@gmail.com>
2016-06-07 21:54:27 -0500

    Create App, and  setupFireBase

---
+++ app/src/main/java/com/example/alice/androidchat/App.java
@@ -0,0 +1,25 @@
+package com.example.alice.androidchat;
+
+import android.app.Application;
+import android.content.res.Configuration;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by alice on 6/7/16.
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        setUpFireBase();
+    }
+
+    private void setUpFireBase() {
+        Firebase.setAndroidContext(this);
+        //  Offline persistence
+        Firebase.getDefaultConfig().setPersistenceEnabled(true);
+    }
+}
